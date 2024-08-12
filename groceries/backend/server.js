0fecd33818f425fafb431b1ac30360506b87c9de
JaccobStanton <jstanton23@my.stlcc.edu>
2024-08-11 23:10:46 -0500

    First commit

---
+++ groceries/backend/server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect("YOUR_MONGODB_CONNECTION_STRING", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const grocerySchema = new mongoose.Schema({
+  name: String,
+  quantity: Number,
+});
+
+const Grocery = mongoose.model("Grocery", grocerySchema);
+
+// CRUD Routes
+
+// Create a grocery
+app.post("/groceries", async (req, res) => {
+  const grocery = new Grocery(req.body);
+  await grocery.save();
+  res.status(201).send(grocery);
+});
+
+// Get all groceries
+app.get("/groceries", async (req, res) => {
+  const groceries = await Grocery.find();
+  res.send(groceries);
+});
+
+// Update a grocery
+app.put("/groceries/:id", async (req, res) => {
+  const grocery = await Grocery.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.send(grocery);
+});
+
+// Delete a grocery
+app.delete("/groceries/:id", async (req, res) => {
+  await Grocery.findByIdAndDelete(req.params.id);
+  res.status(204).send();
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
